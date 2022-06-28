53b8feb538c311075bcb97c65c77068fde67ab0e
Ivo Kriki <ivo.kriki@outlook.com>
2022-06-28 16:56:46 -0300

    create Product class

---
+++ src/main/java/com/productapi/productapi/entity/Product.java
@@ -0,0 +1,25 @@
+package com.productapi.productapi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private LocalDateTime createdAt;
+
+}
